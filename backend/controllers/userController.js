fc7ee38ff369f4972fc952755a2f44c6424cb1d3
muruga21 <murugaperumal21062004@gmail.com>
2024-03-16 01:56:47 +0530

    completed login signup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,80 @@
+const {userModel} = require('../models/userSchema')
+const saltRounds = 10;
+const bcrypt = require('bcrypt');
+const { generateToken } = require('../utils/Token');
+
+const login = async (req, res) => {
+    console.log("check")
+    try{
+        const userName = req.body.userName;
+        const passWord = req.body.password;
+        console.log(userName,passWord)
+        if(userName!="" && passWord!=""){
+            const user = await userModel.findOne({userName:userName})
+            //  console.log(user)
+            if(!user){
+              return  res.status(500).json({error:true,message:"User Not Found"})
+            }
+            
+            const passwordMatch =  await bcrypt.compare(passWord,user.password);
+            // console.log(passwordMatch)
+            if(passwordMatch){
+               const authToken = await generateToken(userName,"user");
+               if(authToken === ""){
+                return res.status(400).json({error:true, message:"auth token not generated"});
+               }
+               res.cookie("token", authToken);
+               return res.status(200).json({error:false,message:{token:authToken,userType: "user"}})
+            }
+            else{
+                return res.status(500).json({error:true,message:"Password not Matching"})
+            }
+        }
+        else{
+            return res.status(500).json({error:true,message:"Enter UserName and PassWord"})
+        }
+
+        
+    }
+    catch(e){
+        return res.status(404).json({error:true,message:e.message})
+
+    }
+}
+
+const signup = async(req, res) =>{
+    const userName = req.body.userName;
+    const email = req.body.email;
+    const password = req.body.password;
+    if(userName == "" || password == "" || email == ""){
+        return res.status(401).json({error: true, message:"invalid credentials"})
+    }
+    if(userName == undefined || password == undefined || email == undefined){
+        return res.status(401).json({error: true, message:"invalid credentials"})
+    }
+    try{
+        const response = await userModel.findOne({userName:userName});
+        if(response){
+            return res.status(401).json({error: true, message:"User Name already exists"});
+        }
+
+        const hashPassword = await bcrypt.hash(password, saltRounds);
+
+        try{
+            const doc = await userModel.create({userName: userName, password: hashPassword, email: email})
+            if(doc){
+                return res.status(200).json({error:false, message:"user created successfully"});
+            }
+        }
+        catch(err){
+            console.log(err.message)
+            return res.status(500).json({error:true, message: err.message})
+        }
+    }
+    catch(err){
+        console.log(err.message)
+        return res.status(520).json({error:true, message: err.message})
+    }
+}
+
+module.exports = {signup, login}
---
+++ backend/utils/Token.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+const secret = process.env.SECRET_KEY;
+
+const generateToken = async(userName, userType) =>{
+    try{
+        const payload = {userName: userName, userType: userType};
+        const accessToken = jwt.sign(payload,secret,{expiresIn: "7D"});
+        // console.log(accessToken)
+        // const decode = jwt.decode(accessToken, secret);
+        // console.log(decode)
+        return accessToken;
+    }
+    catch(err){
+        console.log(err.message);
+        return "";
+    }
+}
+
+module.exports = {generateToken}
