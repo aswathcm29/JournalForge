28239d05076da30b8a0f6f2483bb96e437b13cc3
aswathcm2904 <aswathcm29@gmail.com>
2024-03-16 22:48:58 +0530

    changing about section

--- frontend/src/components/HomePage/About.jsx
+++ frontend/src/components/HomePage/About.jsx
@@ -6,11 +6,12 @@ const About = () => {
     <div>
     <section className='h-[100vh]  p-4 rounded-3xl flex  justify-center'>
       <div className='text-center'>
-        
         <div className=' w-[78rem] h-[40rem] flex justify-center items-center mb-6 rounded-xl py-9 flex-col shadow-xl border-l-4  '>
         <div className='mb-[4rem]'><span className='text-5xl py-2  px-4 bg-green-400 rounded-xl shadow-xl'>About Us</span></div>
-        <p className='text-xl w-[70%]'> At JournalForge, our mission is to empower researchers, scholars, and practitioners worldwide by providing a dynamic platform for the exchange of ideas and the publication of high-quality, peer-reviewed articles. We are committed to fostering academic excellence, promoting interdisciplinary collaboration, and driving innovation forward. Our dedication to advancing knowledge and promoting inclusivity ensures that diverse perspectives are valued and contribute to meaningful discoveries. Through our comprehensive resources and community-driven approach, we strive to create an environment where individuals can engage, learn, and grow together, making a lasting impact on the global academic landscape.
-        </p>
+        <div className='flex flex-row justify-between items-center'>
+        <div className='text-xl px-[2rem] w-[70%]'> At JournalForge, our mission is to empower researchers, scholars, and practitioners worldwide by providing a dynamic platform for the exchange of ideas and the publication of high-quality, peer-reviewed articles. We are committed to fostering academic excellence, promoting interdisciplinary collaboration, and driving innovation forward. Our dedication to advancing knowledge and promoting inclusivity ensures that diverse perspectives are valued and contribute to meaningful discoveries. Through our comprehensive resources and community-driven approach.</div>
+        <img className='w-[20rem] h-[15rem] pr-[3rem] rounded-xl' src={'https://i.pinimg.com/originals/aa/26/92/aa269268f3f7c64bccfefba38dc90944.jpg'} alt='hello'></img>
+        </div>
         </div>
       </div>
     </section>
