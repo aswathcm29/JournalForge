71657efec49322fea4ff03b64c92bbad2d7d6706
muruga21 <murugaperumal21062004@gmail.com>
2024-03-17 17:15:12 +0530

    completed input for journals

--- backend/routes/journalRoutes.js
+++ backend/routes/journalRoutes.js
@@ -1,8 +1,9 @@
 const { addJournal, getJournals } = require('../controllers/journalController');
+const upload = require('../middlewares/multer');
 
 const router = require('express').Router();
 
-router.post('/add', addJournal);
+router.post('/add', upload.single('image'), addJournal);
 router.get('/getJournals', getJournals);
 
 module.exports = router;
