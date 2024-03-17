4485e24aa12cfa65eb7d6d275faf768a7ebbd671
muruga21 <murugaperumal21062004@gmail.com>
2024-03-17 19:29:10 +0530

    asdf

--- backend/routes/journalRoutes.js
+++ backend/routes/journalRoutes.js
@@ -1,4 +1,4 @@
-const { addJournal, getJournals, getUserJournal } = require('../controllers/journalController');
+const { addJournal, getJournals, getUserJournal, getJournalbyId } = require('../controllers/journalController');
 const upload = require('../middlewares/multer');
 
 const router = require('express').Router();
@@ -6,5 +6,6 @@ const router = require('express').Router();
 router.post('/add', addJournal);
 router.get('/getJournals', getJournals);
 router.get('/getUserJournal', getUserJournal);
+router.post('/getJournalbyId', getJournalbyId);
 
 module.exports = router;
