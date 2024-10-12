79c6b1206ae9d764357a8dfb42454027f420fe17
YasirKhan231 <yk4987746@gmail.com>
2024-10-12 11:25:41 +0530

    button hover problem

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function LogoImage() {
+  return (
+    <Link to="/">
+      <span className="text-5xl hover:cursor-pointer select-none">
+        <img src="./fevicon.png" alt="Logo" className="h-24 scale-x-150" />
+      </span>
+    </Link>
+  );
+}
